7ed1d6c12d024b4a109f1dd695457293d2db2c52
calviw001 <calviw8@uci.edu>
2026-04-29 07:06:31 +0000

    Added missing stopwords and fixed mirror errors

--- scraper.py
+++ scraper.py
@@ -16,31 +16,30 @@ version_counts = defaultdict(int)
 page_counts = defaultdict(int)
 
 stopwords = set([
-    "a",          "about",      "above",      "after",
-    "again",      "against",    "all",        "am",
-    "an",         "and",        "any",        "are",
-    "aren't",     "as",         "at",         "be",
-    "because",    "been",       "before",     "being",
-    "below",      "between",    "both",       "but",
-    "by",         "can't",      "cannot",     "could",
-    "couldn't",   "did",        "didn't",     "do",
-    "does",       "doesn't",    "doing",      "don't",
-    "down",       "during",     "each",       "few",
-    "for",        "from",       "further",    "had",
-    "hadn't",     "has",        "hasn't",     "have",
-    "haven't",    "having",     "he",         "he'd",
-    "he'll",      "he's",       "her",        "here",
-    "here's",     "hers",       "herself",    "him",
-    "himself",    "his",        "how",        "how's",
-    "i",          "i'd",        "i'll",       "i'm",
-    "i've",       "if",         "in",         "into",
-    "is",         "isn't",      "it",         "it's",
-    "its",        "itself",     "let's",      "me",
-    "more",       "most",       "mustn't",    "my",
-    "myself",     "no",         "nor",        "not",
-    "of",         "off",        "on",         "once",
-    "only",       "or",         "other",      "ought",
-    "our",        "ours"
+    "a", "about", "above", "after", "again", "against", "all", "am",
+    "an", "and", "any", "are", "aren't", "as", "at", "be", "because",
+    "been", "before", "being", "below", "between", "both", "but",
+    "by", "can't", "cannot", "could", "couldn't", "did", "didn't",
+    "do", "does", "doesn't", "doing", "don't", "down", "during",
+    "each", "few", "for", "from", "further", "had", "hadn't", "has",
+    "hasn't", "have", "haven't", "having", "he", "he'd", "he'll",
+    "he's", "her", "here", "here's", "hers", "herself", "him",
+    "himself", "his", "how", "how's", "i", "i'd", "i'll", "i'm",
+    "i've", "if", "in", "into", "is", "isn't", "it", "it's", "its",
+    "itself", "let's", "me", "more", "most", "mustn't", "my", "myself",
+    "no", "nor", "not", "of", "off", "on", "once", "only", "or",
+    "other", "ought", "our", "ours", "ourselves", "out", "over",
+    "own", "same", "shan't", "she", "she'd", "she'll", "she's",
+    "should", "shouldn't", "so", "some", "such", "than", "that",
+    "that's", "the", "their", "theirs", "them", "themselves", "then",
+    "there", "there's", "these", "they", "they'd", "they'll",
+    "they're", "they've", "this", "those", "through", "to", "too",
+    "under", "until", "up", "very", "was", "wasn't", "we", "we'd",
+    "we'll", "we're", "we've", "were", "weren't", "what", "what's",
+    "when", "when's", "where", "where's", "which", "while", "who",
+    "who's", "whom", "why", "why's", "with", "won't", "would",
+    "wouldn't", "you", "you'd", "you'll", "you're", "you've", "your",
+    "yours", "yourself", "yourselves"
 ])
 
 def scraper(url, resp):
@@ -183,7 +182,7 @@ def is_page_duplicate(page_text):
 def is_too_large(resp):
     # Return true if the file is too large, and return false otherwise
     if not resp.raw_response or not resp.raw_response.content:
-        return false
+        return False
     return len(resp.raw_response.content) > 10_000_000 
 
 def word_is_valid(word):
