603bf36fd7df51fdecf9811d096dd4078072f6f5
calviw001 <calviw8@uci.edu>
2026-04-29 18:00:53 +0000

    Deleted duplicated code part

--- launch.py
+++ launch.py
@@ -61,16 +61,6 @@ def main(config_file, restart):
         end_time = time.perf_counter()
         total_runtime = end_time - start_time
         output_stats("report.txt")
-    start_time = time.perf_counter()
-    try:
-        crawler.start()
-    except KeyboardInterrupt:
-        print("keyboard interrupt")
-    finally:
-        end_time = time.perf_counter()
-        total_runtime = end_time - start_time
-        output_stats("report.txt")
-
 
 if __name__ == "__main__":
     parser = ArgumentParser()
